8b64d82b09b2cfd613336b6fa8f91618fa1bb277
Andrea Almiron <andreamontes.rei@gmail.com>
2023-03-17 15:02:02 -0600

    cleaned up code

--- script.js
+++ script.js
@@ -12,29 +12,17 @@ var history = '';
         console.log(location.hash);
         var innerElement = '';
 
-        // get requested page
         var link = window.location.hash;
-
-        // ----------------------------------------
-        // If more than one parameter in the link, 
-        // I am targeting an element in the page, 
-        // an anchor. First load page, the scroll
-        // the element into view.
-        // ----------------------------------------
 
         var count = (link.split("/").length - 1);        
         if (count > 1) {
-            // anchor element 
+            
             innerElement = link.split("/")[2];            
 
-            // page to load
             link = '#/' + link.split("/")[1];
         }
 
-        // ----------------------------------------
-        // Remember loaded page - used to avoid
-        // page reload on internal linking
-        // ----------------------------------------        
+        // Remember history to avoid reload        
         if (history === link && innerElement){
             scrollIntoView(innerElement);
             history = link;
@@ -42,10 +30,8 @@ var history = '';
         }
         history = link;  
 
-        // get path (route) for page
         var route = routes[link];
 
-        // if route exists, load page
         if (route) loadPage(route, innerElement);
     };
     router();
@@ -57,11 +43,7 @@ var history = '';
         const element = document.getElementById('content');
         element.innerHTML = content;
 
-        // ------------------------------------------
-        // Scroll to top -- need to avoid navigation 
-        // drift on page. Else the scroll state 
-        // carries over on to next page
-        // ------------------------------------------
+        //Scroll
         window.scrollTo(0, 0);
 
 
